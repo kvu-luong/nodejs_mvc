19e2a729b9d9e9dfa4fb5cf30321d16d7ed4a1c9
root <luongkhanhvu1392@gmail.com>
2022-04-15 14:43:55 +0700

    setup(server): add https to make ssl config

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
+const fs = require('fs');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const https = require('https'); 
+const options = {
+   key  : fs.readFileSync('/etc/letsencrypt/live/v2vbro.com-0001/privkey.pem', 'utf8'),
+   cert : fs.readFileSync('/etc/letsencrypt/live/v2vbro.com-0001/cert.pem', 'utf8')
+};
+
 const express = require('express');
 const app = express();
 app.set('view engine', 'ejs');
-const PORT = process.env.PORT || 3000;
+app.use(bodyParser.urlencoded({ extended: false}));//disbale parsing application/x-www-form-urlencoded
+app.use(bodyParser.json({limit: '1mb'})); // for req.body
+app.use(cors({
+  origin: '*',
+  allowedHeaders: 'x-access-token',
+}));
 
-//Routes
-app.use('/', require('./routes/login'));
-
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 8082;
+const server = https.createServer(options, app);
+server.listen(PORT, () => {
   console.log(`Server start for port: ${PORT}`);
 });
+
+//---------Controller----------
+const home = require('./routes/login');
+//Routes
+app.use('/', home);
+app.use((req, res, next) => {
+  res.status(404).send({
+    status: 404,
+    error: `API_NOT_FOUND`,
+  })
+});
+
+
+
+
+
